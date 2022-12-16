f4eccad2c61f96ec5d92c40b09de1ad84e2dfe42
Romaso1 <90009050+Romaso1@users.noreply.github.com>
2022-12-17 00:34:12 +0200

    1
    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Head from './components/Head';
+
+function App() {
+  return (
+    <div>
+     <Head />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './components/css/css-aos.css';
+import './components/css/css-bootstrap-datepicker.css';
+import './components/css/css-bootstrap.min.css';
+import './components/css/css-jquery-ui.css';
+import './components/css/css-jquery.fancybox.min.css';
+import './components/css/css-owl.carousel.min.css';
+import './components/css/css-owl.theme.default.min.css';
+import './components/css/css-style.css';
+import './components/css/font-flaticon.css';
+import './components/css/icomoon-style.css';
+
+//<link href="https://fonts.googleapis.com/css?family=Oswald:400,700%7CMuli:300,400,700,900" rel="stylesheet"></link>
+
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+  crossorigin="anonymous"
+/>
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+
+<script src="./js/602-js-jquery-3.3.1.min.js"></script>
+<script src="./js/9980-js-jquery-ui.js"></script>
+<script src="./js/7633-js-popper.min.js"></script>
+<script src="./js/4046-js-bootstrap.min.js"></script>
+<script src="./js/6829-js-owl.carousel.min.js"></script>
+<script src="./js/6062-js-jquery.easing.1.3.js"></script>
+<script src="./js/2817-js-aos.js"></script>
+<script src="./js/1169-js-jquery.fancybox.min.js"></script>
+<script src="./js/9251-js-jquery.waypoints.min.js"></script>
+<script src="./js/6367-js-jquery.animateNumber.min.js"></script>
+<script src="./js/6658-js-jquery.sticky.js"></script>
+<script src="./js/8180-js-main.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"77a1890efa3ec6a5","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2022.11.3","si":100}' crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" 
+integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" 
+crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" 
+integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" 
+crossorigin="anonymous"></script>
+
+
+
+  </React.StrictMode>
+  
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
