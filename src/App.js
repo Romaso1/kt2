05d45ae27a82941cb36c6d9779ec01530d273935
Romaso1 <90009050+Romaso1@users.noreply.github.com>
2022-12-17 23:55:51 +0200

    1
    Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Head from './components/Head';
-import Head2 from './components/Head2';
+import Mid from './components/Mid';
+import Bot from './components/Bottom';
 
 function App() {
   return (
     <div>
      <Head />
+     <Mid />
+     <Bot />
     </div>
     
   );
